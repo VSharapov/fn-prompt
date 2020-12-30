efba7cd9cacda5c56393cef8396241ddcdfe09d8
Vasiliy Sharapov <vasiliys@gmail.com>
2020-12-30 11:23:05 -0500

    Comments/clarification

--- fn_prompt.sh
+++ fn_prompt.sh
@@ -3,30 +3,40 @@
 #notify-send "Fn"
 #set -x
 
+# Should these live in /tmp ? ... dunno
 lock_file="${HOME}/.fn_prompt"
 resp_file="${HOME}/.fn_response"
 for i in $lock_file $resp_file; do
 	touch $i;
 done
 
+# Explanation of lock_file magic values:
+#     0 - Fn has NOT been pressed recently
+#     1 - Fn HAS been pressed recently
+#     2 - xTerm is open, waiting for a command
 initial_value="$(cat $lock_file)"
 
+# From 0 to 1...
 if [ "$initial_value" == "0" ]; then
 	echo "1" > $lock_file
 	sleep 1
+	# ... and if it's still 1, back to 0
 	new_value="$(cat $lock_file)"
 	if [ $new_value == "1" ]; then
 		echo 0 > $lock_file
 	fi
 fi
 
+# From 1 to 2...
 if [ "$initial_value" == "1" ]; then
 	echo 2 > $lock_file
 	if [ -e $resp_file ]; then rm $resp_file; fi
 	# TODO: The next line does not block sometimes (for example if a 't' is open) and everything after it doesn't get run
+	# TODO: Wait... I don't think this ^^^ is true.
 	xterm -e bash -c "echo 'Enter command'; read -n 1 -t 3 && echo \$REPLY > $resp_file"
 	command="$(if [ -e $resp_file ]; then cat $resp_file; fi)"
 	#notify-send "$(cat $resp_file)"
+	# ... and hopefully this is always true
 	new_value="$(cat $lock_file)"
 	if [ $new_value == "2" ]; then
 		echo 0 > $lock_file
@@ -39,17 +49,21 @@ case "$command" in
 #		eog -f "${HOME}/Desktop/test/afk.png" &
 #		;;
 	"b")
+		# Blank screen
 		sleep 1; xset dpms force off &
 		;;
 	"d")
 		~/Downloads/DiscordPTB/DiscordPTB &
 		;;
 	"e")
+		# Edit this file
 		gedit "$0" &
 		;;
 	"h")
+		# Headphones
 		echo -e "connect 88:D0:39:6F:81:40" | bluetoothctl
 		echo -e "connect FC:58:FA:F5:89:84" | bluetoothctl
+		# # Hacker news
 		# firefox "https://news.ycombinator.com/" &
 		;;
 #	"j")
@@ -62,6 +76,7 @@ case "$command" in
 #		notify-send "$(otp.py)" &
 #		;;
 	"s")
+		# SILENCE!
 		killall totem-audio-preview
 		killall festival
 		killall espeak
@@ -69,6 +84,7 @@ case "$command" in
 		killall play
 		;;
 	"t")
+		# Bigterm
 		gnome-terminal --maximize --hide-menubar &
 		;;
 	"x")
